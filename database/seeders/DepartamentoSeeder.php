e161a60a886eee87777d29ed3fa76cf156f109b6
Ronald Niz <ronaldalexisniznunez@gmail.com>
2024-09-23 12:43:43 -0400

    campo (DEPORTES) añadido al Seeder Departento

--- database/seeders/DepartamentoSeeder.php
+++ database/seeders/DepartamentoSeeder.php
@@ -22,6 +22,7 @@ class DepartamentoSeeder extends Seeder
             'COMUNICACIONES',
             'COMISIONES VECINALES',
             'CULTURA',
+            'DEPORTES',
             'DESARROLLO HUMANO',
             'DESARROLLO SOCIAL',
             'DISCAPACIDAD',
