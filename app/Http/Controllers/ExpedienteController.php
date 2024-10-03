c69d35c6e6b53612c433207c7039948949d7d147
Ronald Niz <ronaldalexisniznunez@gmail.com>
2024-10-03 09:04:56 -0400

    Vista de panel modificada

--- app/Http/Controllers/ExpedienteController.php
+++ app/Http/Controllers/ExpedienteController.php
@@ -8,22 +8,11 @@ use Illuminate\Support\Facades\Storage;
 
 class ExpedienteController extends Controller
 {
-    // public function downloada($record)
-    // {
-    //     $data = Archivo::select('id', 'ruta', 'nombre_original')->where('id', $record)->first();
-
-    //     $content = Storage::disk('public')->path($data->ruta_archivo);
-
-    //     return response()->download($content);
-    // }
-
     public function download($record)
     {
         $archivo = Archivo::findOrFail($record);
 
         $content = Storage::disk('public')->path($archivo->ruta);
-        
-
         
         return response()->download($content, $archivo->nombre_original);
     }
